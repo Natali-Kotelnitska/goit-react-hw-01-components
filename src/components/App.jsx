7b738409f98ba0f9a528f297dcc7a9f59a926470
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-05-02 04:15:33 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -36,12 +36,9 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      {/* <Statistics stats={data} />; */}
       <FriendList friends={friends} />
 
       <TransactionHistory items={transactions} />
-
-      {/* <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
